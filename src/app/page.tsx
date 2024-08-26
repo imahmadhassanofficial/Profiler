3c3a8f07d7456fd1a551ddc79414488f37230911
imahmadhassanofficial <hereahmadhassan@gmail.com>
2024-08-26 23:59:41 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<h1 className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius labore optio amet? Porro dolores repellendus cumque accusamus doloribus unde, nam error consequatur soluta?</h1>
+<h1 className="text-black flex justify-center align-center text-[2.5vw]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius labore optio amet? Porro dolores repellendus cumque accusamus doloribus unde, nam error consequatur soluta?</h1>
   );
 }
