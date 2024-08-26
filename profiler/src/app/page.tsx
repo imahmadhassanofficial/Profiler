7315538e0e8a0aae34c726833105213dffd144d3
imahmadhassanofficial <hereahmadhassan@gmail.com>
2024-08-26 23:53:22 +0500

    fixes

--- profiler/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
- <h1 className="text-[2vw] text-gray-500 justify-center display-flex text-center align-center"> Hello World </h1>
- 
-  );
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<h1 className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius labore optio amet? Porro dolores repellendus cumque accusamus doloribus unde, nam error consequatur soluta?</h1>
+  );
+}
