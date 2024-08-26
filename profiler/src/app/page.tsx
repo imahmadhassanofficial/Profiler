62497218fb2566712c9fa960a2855cfac5c00f62
imahmadhassanofficial <hereahmadhassan@gmail.com>
2024-08-26 23:50:19 +0500

    test123

--- profiler/src/app/page.tsx
+++ profiler/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
- <h1>Hello World </h1>
+ <h1 className="text-[2vw] text-gray-500 justify-center display-flex text-center align-center"> Hello World </h1>
+ 
   );
 }
